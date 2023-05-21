87f357b2f7171366a96e73aead6db88cb912d15d
AndreiBacila <andrei.bacila@rodeapps.com>
2023-05-21 13:50:44 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/layout/Header";
+
+function App() {
+    return (
+        <>
+            <Header></Header>
+            <main>
+                <p>Welcome to my app</p>
+            </main>
+            <footer>
+                <p>Goodbye</p>
+            </footer>
+        </>
+    );
+}
+
+export default App;
