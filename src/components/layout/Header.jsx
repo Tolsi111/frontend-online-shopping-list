31699a8ca07ceaa9488453ff5fa7bf922b4a3930
AndreiBacila <30293758+Tolsi111@users.noreply.github.com>
2023-08-02 22:37:35 +0300

    Added Home nav button

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,21 @@
 import classes from './Layout.module.css'
+import NavButton from "../NavButton";
 
 function Header() {
+    function handleClick() {
+
+    }
+
     return (
         <>
             <header className={classes.header}>
-                <h1>Online shopping cart</h1>
+                <h1 className={classes.title}>Online shopping cart</h1>
+                <div className={classes.buttonContainer}>
+                    <NavButton className={classes.homeButton} title={'Home'} navigateTo={"/"}/>
+                </div>
+                {/*<div className={classes.homeButton} onClick={handleClick}>*/}
+                {/*    <p>Home</p>*/}
+                {/*</div>*/}
                 {/*{authCtx.isLoggedIn && <p>Hi, user</p>}*/}
                 {/*{authCtx.isLoggedIn && <NavButton type={"submit"}> logout</NavButton>}*/}
             </header>
